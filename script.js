5ddafb9c9d2344cf8a98cea1b9df2c9e4e29b999
NoTheRaj <prithviraj.fry0u@gmail.com>
2026-03-23 09:21:57 +0530

    Milestone 1: static layout shell

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+// Milestone 1: No JavaScript behavior yet.
+// This file exists so the HTML link does not throw a 404 error in the browser console.
+// State switching, chip selection, and result mode will be added in Milestone 2.
